e83bec6ca3924e9e9ea216b1236c76620c5b03a9
AtharvGaur <atharava112@gmail.com>
2025-02-24 16:21:21 +0530

    Enhance authentication middleware and sign-in page; add user registration logic

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,18 @@
 import { SignIn } from '@clerk/nextjs'
+import Image from 'next/image'
+import React from 'react'
 
-export default function Page() {
-  return <SignIn />
+function page() {
+  return (
+    <div className='flex min-h-screen items-center justify-center gap-40 '>
+      <div className='hidden md:block'>
+        <Image src={'/img2.jpg'} height={500} width={500}></Image>
+      </div>
+      <div>
+        <SignIn/>
+      </div>
+    </div>
+  )
 }
+
+export default page
--- app/Provider.js
+++ app/Provider.js
@@ -1,6 +1,28 @@
-import React from 'react'
+"use client"
+import { Users } from '@/config/schema';
+import { useUser } from '@clerk/nextjs';
+import React, { useEffect } from 'react'
 
 function Provider({ children }) {
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && isNewuser();
+  }, [user])
+
+  const isNewuser = async () => {
+    const result = await db.select().from(Users)
+    .where(eq(Users.email.user?.primaryEmailAddress?.emailAddress));
+    console.log(result);
+    if (!result[0]){
+      await db.insert(Users).values({
+        name: user.fullName,
+        email: user?.primaryEmailAddress?.emailAddress,
+        imageUrl: user?.imageUrl
+      })
+    }
+  }
+
   return (
     <div>
         {children}
