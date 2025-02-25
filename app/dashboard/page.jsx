e03b7326d087a797992ea77bcf74b63ea31676ae
AtharvGaur <atharava112@gmail.com>
2025-02-25 15:55:52 +0530

    Add EmptyState component and enhance dashboard layout; implement video list state management

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import EmptyState from './_components/EmptyState';
 
 function page() {
+  const [ videoList, setVideoList ] = useState([]);
+
   return (
-    <div>page</div>
+    <div>
+      <div className='flex justify-between items-center'>
+        <h2 className='font-bold text-2xl text-primary'>Dashboard</h2>
+        <Button>+ Create new</Button>
+      </div>
+
+      {videoList?.length == 0 && <div>
+        <EmptyState/>
+      </div> }
+    </div>
   )
 }
 
