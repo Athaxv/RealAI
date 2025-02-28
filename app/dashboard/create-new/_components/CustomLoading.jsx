893f4719186fc66b9e3cc0a8e89da24f2a36528f
AtharvGaur <atharava112@gmail.com>
2025-02-28 19:57:29 +0530

    Add CustomLoading component and integrate Radix UI Alert Dialog; enhance loading state

---
+++ app/dashboard/create-new/_components/CustomLoading.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+import Image from 'next/image'
+  
+
+function CustomLoading({loading}) {
+  return (
+    <AlertDialog open={loading}>
+  {/* <AlertDialogTrigger>Open</AlertDialogTrigger> */}
+  <AlertDialogContent className="w-[200px] rounded-md h-40">      
+    <div className='bg-white flex flex-col items-center justify-center'>
+    <Image src={'/truck.gif'} height={50} width={50} alt='truck'/>
+    <h2>Generating...</h2>
+    </div>
+  </AlertDialogContent>
+</AlertDialog>
+
+  )
+}
+
+export default CustomLoading
--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -5,9 +5,13 @@ import SelectStyle from './_components/SelectStyle';
 import SelectDuration from './_components/SelectDuration';
 import { Button } from '@/components/ui/button';
 import axios from 'axios';
+import CustomLoading from './_components/CustomLoading';
 
 function page() {
+    const [loading, setLoading] = useState(false);
     const [formData, setFormData] = useState({});
+    const [videoScript, setVideoScript] = useState()
+
     const onHandleInputChange = (fieldName, fieldValue) => {
         console.log(fieldName, fieldValue)
         setFormData(prev => ({
@@ -18,14 +22,20 @@ function page() {
 
     const handleSubmit = () => {
         GetVideoScript();
+        // setLoading(true);
     }
 
     const GetVideoScript = async () => {
+        setLoading(true);
         const prompt = 'Write a script to generate a '+formData.duration+' video on topic: Interesting '+formData.topic+' along with AI image prompt in'+formData.imageStyle+' format for each scene and give me result in JSON format with imagePrompt and contentText as field, no plain text'
         console.log(prompt)
         const result = await axios.post('/api/get-video-script', {
             prompt: prompt,
-        }).then((res) => console.log(res.data))
+        }).then((res) => {
+            console.log(res.data.result);
+            setVideoScript(res.data.result);
+        })
+        setLoading(false);
     }
   return (
     <div className='md:px-20'>
@@ -36,6 +46,7 @@ function page() {
             <SelectDuration onUserSelect={onHandleInputChange}/>
             <Button className="mt-5 w-full" onClick={handleSubmit}>Create short</Button>
         </div>
+        <CustomLoading loading={loading}/>
     </div>
   )
 }
